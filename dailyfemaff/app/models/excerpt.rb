2dd65a7b78e4c7a66ebe7b85766c70618f1594dc
Hilary Stohs-Krause <hilarysk@gmail.com>
2015-03-18 10:57:20 -0500

    added excerpts table (i think)

---
+++ dailyfemaff/app/models/excerpt.rb
@@ -0,0 +1,13 @@
+class Videogame < ActiveRecord::Base
+  include FeministInstanceMethods
+  
+  attr_accessible :excerpt, :source, :person_id, :user_id, :created_at, :updated_at
+  
+  belongs_to :person
+  belongs_to :user
+  
+  has_many :keyword_items, as: :item
+
+  validates :excerpt, uniqueness: { case_sensitive: false }
+  validates :excerpt, :source, presence: true  
+end
---
+++ dailyfemaff/db/migrate/20150318155619_change_excerpt_class.rb
@@ -0,0 +1,7 @@
+class ChangeExcerptClass < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
